3b0a75377c834e5b9145c30894a04d53cc295086
nomoto_r <nomoto_r@worksap.co.jp>
2020-01-10 20:36:01 +0900

    Stacking defers

--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
-
-func Sqrt(x float64) float64 {
-	z := x / 2
-	for i := 1; i <= 10; i++ {
-		prev := z
-		z -= (z*z - x) / (2 * z)
-		fmt.Println("count: ", i)
-		fmt.Println("z: ", z)
-		if (prev - z) == 0 {
-			break
-		}
-	}
-	return z
-}
+import "fmt"
 
 func main() {
-	fmt.Println("result", Sqrt(100))
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
