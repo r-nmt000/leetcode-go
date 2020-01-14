dd7c44b90b6649376f0caaeeb7bf61de53d6b02e
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-01-14 19:54:35 +0900

    Exercise: Slices

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import "golang.org/x/tour/pic"
 
+func Pic(dx, dy int) [][]uint8 {
+    image := make([][]uint8, dy)
+    for i := range image {
+        image[i] = make([]uint8, dx)
+        for j := range image[i] {
+            image[i][j] = uint8(i*j)
+        }
+    }
+    return image
+}
 func main() {
-	fmt.Println("counting")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-	fmt.Println("done")
+    pic.Show(Pic)
 }
