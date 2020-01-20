040e25ee6e782f68bd8715c40338df0a822c4424
nomoto_r <nomoto_r@worksap.co.jp>
2020-01-20 19:55:31 +0900

    Exercise: Fibonacci closure

--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
 )
 
-type Abser interface {
-    Abs() float64
-    Test() float64
-}
-
-type Vertex struct {
-    X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-    return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-    if f < 0 {
-        return float64(-f)
-    }
-    return float64(f)
-}
-
-func (f MyFloat) Test() float64 {
-    return 0
-}
-
 func main() {
-    var a Abser
-    f := MyFloat(-math.Sqrt2)
-    v := Vertex{3, 4}
+	var i interface{}
+	describe(i)
 
-    a = f
-    a = &v
+	i = 42
+	describe(i)
 
-    a = v
+	i = "hello"
+	describe(i)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
