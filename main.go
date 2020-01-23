d9de10edf86ae39a69e0632fce1ff50d285405d6
nomoto_r <nomoto_r@worksap.co.jp>
2020-01-23 19:45:31 +0900

    Exercise: Stringers

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
+type IPAddr [4]byte
+
+func (ipAddr IPAddr) String() string {
+	return fmt.Sprintf("[%v.%v.%v.%v]", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3])
 }
+
 func main() {
-	do(21)
-	do("hello")
-	do(true)
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
