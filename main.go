ff8d215a9b9f8036831daec18ca3bb2564cac0a5
nomoto_r <nomoto_r@worksap.co.jp>
2020-01-24 19:48:37 +0900

    Readers

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"strings"
 )
 
-type ErrNegativeSqrt float64
-
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
-}
-
-func Sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
-	}
-	z := 1.0
-	for i := 0; i < 10; i++ {
-		z -= (z*z - x) / (2 * z)
-	}
-	return z, nil
-}
-
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v, err = %v, b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
