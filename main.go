654661607ca5cf3496231cac2feb9115c67c4d5a
nomoto_r <nomoto_r@worksap.co.jp>
2020-01-24 19:27:15 +0900

    Exercise: Errors

--- main.go
+++ main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type MyError struct {
-	When time.Time
-	What string
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
 }
 
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %v", e.When, e.What)
-}
-
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
 	}
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
 }
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
