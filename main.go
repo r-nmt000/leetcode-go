7992557fd3feb40fd6a4ca54477f51b116def897
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-01-25 17:00:30 +0900

    Channels

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
     "fmt"
-    "time"
 )
 
-func say(s string) {
-    for i := 0; i < 5; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Println(s)
+func sum(s []int, c chan int) {
+    sum := 0
+    for _, v := range s {
+        sum += v
     }
+    c <- sum
 }
 
 
 func main() {
-    go say("world")
-    say("hello")
+    s := []int{7,2,8,-9, 4, 0}
+
+    c := make(chan int)
+    go sum (s[:len(s)/2], c)
+    go sum(s[len(s)/2:], c)
+    x, y := <-c, <-c
+
+    fmt.Println(x, y, x+y)
 }
