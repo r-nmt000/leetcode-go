0e07e8044f99960d6b457bf4cbdc31560832420a
nomoto_r <r.nmt000@gmail.com>
2020-01-29 16:13:05 +0900

    longest common prefix

---
+++ longest-common-prefix/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"aaa", "aabab", "aac"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for strings.Index(strs[i], prefix) == -1 {
+			prefix = prefix[0 : len(prefix)-1]
+		}
+	}
+	return prefix
+}
