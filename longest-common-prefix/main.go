34ccbe199807f50e676c3f5c033f3ebc8c7db28d
nomoto_r <r.nmt000@gmail.com>
2020-01-29 16:16:43 +0900

    longest common prefix

--- longest-common-prefix/main.go
+++ longest-common-prefix/main.go
@@ -10,6 +10,9 @@ func main() {
 }
 
 func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
 	prefix := strs[0]
 	for i := 1; i < len(strs); i++ {
 		for strings.Index(strs[i], prefix) == -1 {
